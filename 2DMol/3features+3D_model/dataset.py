dc5e84dd5e424f086468315db643d5ceee5440b7
Computational Energetics Laboratory <zhoukenwpu@163.com>
2022-01-29 17:06:39 +0800

    Update dataset.py

--- 2DMol/3features+3D_model/dataset.py
+++ 2DMol/3features+3D_model/dataset.py
@@ -24,9 +24,9 @@ class ModelNetDataset(data.Dataset):
         self.fns = []
         with open(os.path.join(root, 'owndata_{}.txt'.format(self.split)), 'r') as f:
             for line in f:
-                self.fns.append(line.strip())    #line.strip()删除末尾结束符
+                self.fns.append(line.strip())    # line.strip() is using to remove end terminator
  
-        self.cat = {}   #calss
+        self.cat = {}   #class
         with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '/data/home/zhaoyj/01ML/pointnet.pytorch.3D-v2/misc/owndata_id.txt'), 'r') as f:
             for line in f:
                 ls = line.strip().split()
@@ -35,17 +35,15 @@ class ModelNetDataset(data.Dataset):
         self.classes = list(self.cat.keys())
  
     def __getitem__(self, index):
-        file_index = self.fns[index]        #eg: index:845 -> bed_0114
-        cls_name = file_index.rsplit(sep='_',maxsplit=1)[0]    # eg: airplane
-        cls = self.cat[cls_name]    #eg: out:[0]    airplane belongs to [0],bench belongs to [3]
-        file_name = '{}/{}.txt'.format(cls_name,file_index)  #eg: airplane/airplane_0001.txt
+        file_index = self.fns[index]        #eg: index:4 -> interphase_1087
+        cls_name = file_index.rsplit(sep='_',maxsplit=1)[0]    # eg: crumple
+        cls = self.cat[cls_name]    # eg: out:[0]; interphase belongs to [0], quasiflat belongs to [3]
+        file_name = '{}/{}.txt'.format(cls_name,file_index)  # eg: fold/fold_1021.txt
         pts = np.loadtxt(os.path.join(self.root,file_name),delimiter=' ',dtype=float)[:,1:4]
-        #随机降采样，提高模型的鲁棒性
+        # Random downsampling to improve model robustness
         choice = np.random.choice(len(pts), self.npoints, replace=True)
         #numpy.random.choice(a, size=None, replace=True, p=None)
-        #从a(只要是ndarray都可以，但必须是一维的)中随机抽取数字，并组成指定大小(size)的数组
-        #replace:True表示可以取相同数字，False表示不可以取相同数字
-        #数组p：与数组a相对应，表示取数组a中每个元素的概率，默认为选取每个元素的概率相同。
+
  
         point_set = pts[choice, :]
  
@@ -92,7 +90,7 @@ if __name__ == '__main__':
         num_workers=4
     )
     for epoch in range(10):
-        for step,data in enumerate(dataloder,0):     #0表示从索引0开始
+        for step,data in enumerate(dataloder,0):     
             points , target = data
             target = target[:,0]
             print('Epoch: ', epoch, '| Step: ', step, '| points: ',
